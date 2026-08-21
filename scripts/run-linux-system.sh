5bb58f3912f9fb56d86e253671c67942e4f96b66
max <max@blue.tools>
2026-08-22 00:25:41 +0900

    Provide Linux system resize headroom

--- scripts/run-linux-system.sh
+++ scripts/run-linux-system.sh
@@ -12,7 +12,7 @@ set -eu
 test -x /bin/bash
 export SHELL=/bin/bash
 
-dbus-run-session -- xvfb-run -a -s "-screen 0 1400x900x24" sh -eu -c '
+dbus-run-session -- xvfb-run -a -s "-screen 0 1920x1200x24" sh -eu -c '
   printf "\n" | gnome-keyring-daemon --unlock >/tmp/soksak-keyring.env
   openbox >/tmp/soksak-openbox.log 2>&1 &
   export GDK_BACKEND=x11
